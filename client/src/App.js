fa892f94e9dfcda99db61570abfc69fde25c5dea
Yousef Wael <yousef.wael2002@gmail.com>
2024-03-01 04:55:17 +0200

    finished post form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import './style.css'
 import Header from './Header';
 import BoardHeader from './BoardHeader';
+import PostForm from './PostForm';
+
 
 function App() {
   return (
     <div>
       <Header />
       <BoardHeader />
+      <PostForm />
+      
+
     </div>
   );
 }
